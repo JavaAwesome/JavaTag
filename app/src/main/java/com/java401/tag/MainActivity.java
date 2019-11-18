26c2cfd6978ce0df58625b70ba682308925a2be7
ahren <ahrenswett@gmail.com>
2019-11-18 10:36:03 -0800

    refresh

---
+++ app/src/main/java/com/java401/tag/MainActivity.java
@@ -0,0 +1,14 @@
+package com.java401.tag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
