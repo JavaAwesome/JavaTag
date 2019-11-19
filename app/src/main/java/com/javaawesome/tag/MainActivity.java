ae207d5ca8acd06529d94fa861e510d010a8a18a
ahrenswett <ahrenswett@gmail.com>
2019-11-19 09:16:27 -0800

    Revert "Camera"

--- app/src/main/java/com/javaawesome/tag/MainActivity.java
+++ app/src/main/java/com/javaawesome/tag/MainActivity.java
@@ -22,54 +22,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // initialize aws mobile client and check if you are logged in or not
-//        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
-//            @Override
-//            public void onResult(UserStateDetails result) {
-//                // if the user is signed out, show them the sign in page
-//                if (result.getUserState().toString().equals("SIGNED_OUT")) {
-//                    signInUser();
-//                }
-//            }
-//
-//            @Override
-//            public void onError(Exception e) {
-//                Log.e(TAG, e.getMessage());
-//            }
-//        });
-    }
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails result) {
+                // if the user is signed out, show them the sign in page
+                if (result.getUserState().toString().equals("SIGNED_OUT")) {
+                    signInUser();
+                }
+            }
 
-    public void gotocamera(View view){
-        Intent gotoCameraIntent= new Intent(this,ShowMeYourFace.class);
-        this.startActivity(gotoCameraIntent);
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+        });
     }
-
 
     public void goToMap(View view) {
         Intent goToMapIntent = new Intent(this, MapsActivity.class);
         this.startActivity(goToMapIntent);
     }
 
-//    // Direct users to sign in page
-//    public void signInUser() {
-//        AWSMobileClient.getInstance().showSignIn(MainActivity.this,
-//                // customize the built in sign in page
-//                SignInUIOptions.builder().backgroundColor(16763080).build(),
-//                new Callback<UserStateDetails>() {
-//                    @Override
-//                    public void onResult(UserStateDetails result) {
-//                        Log.i(TAG, "successfully show signed in page");
-//                    }
-//
-//                    @Override
-//                    public void onError(Exception e) {
-//                        Log.e(TAG, e.getMessage());
-//                    }
-//                });
-//    }
+    // Direct users to sign in page
+    public void signInUser() {
+        AWSMobileClient.getInstance().showSignIn(MainActivity.this,
+                // customize the built in sign in page
+                SignInUIOptions.builder().backgroundColor(16763080).build(),
+                new Callback<UserStateDetails>() {
+                    @Override
+                    public void onResult(UserStateDetails result) {
+                        Log.i(TAG, "successfully show signed in page");
+                    }
 
-//    // sign out user and show them sign in page
-//    public void signoutCurrentUser(View view) {
-//        AWSMobileClient.getInstance().signOut();
-//        signInUser();
-//    }
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                });
+    }
+
+    // sign out user and show them sign in page
+    public void signoutCurrentUser(View view) {
+        AWSMobileClient.getInstance().signOut();
+        signInUser();
+    }
 }
